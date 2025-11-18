9860489fb47d624556da4e6496bebf3b0c8035cb
nitikaarya21 <nitika3126@gmail.com>
2025-11-18 22:48:51 +0530

    updtae env

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Product } from '@/components/ProductCard'
 
 
 async function getProducts(): Promise<Product[]> {
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000'
+const baseUrl = 'http://localhost:3000'
 const res = await fetch(`${baseUrl}/api/products`, { cache: 'no-store' })
 if (!res.ok) return []
 return res.json()
